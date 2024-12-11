f3854369712529644712b342c48545e2c52f7050
Giovanna Scalfoni Sales <104603818+gscalfoni@users.noreply.github.com>
2024-12-11 14:12:47 +0000

    teste

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ def int_to_roman(num):
         raise ValueError("Número fora do intervalo válido (1 a 3999)")
 
     roman_numerals = [
-        (3000, "M"), (900, "CM"), (500, "D"), (400, "CD"),
+        (2000, "M"), (900, "CM"), (500, "D"), (400, "CD"),
         (100, "C"), (90, "XC"), (50, "L"), (40, "XL"),
         (10, "X"), (9, "IX"), (5, "V"), (4, "IV"), (1, "I")
     ]
